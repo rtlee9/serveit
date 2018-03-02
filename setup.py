261ea8d087464c9fbf641130c8c385a4d522881a
Ryan Lee <ryantlee9@gmail.com>
2018-03-01 22:45:12 -0800

    Add meinheld to setup requirements

--- setup.py
+++ setup.py
@@ -126,7 +126,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['flask', 'flask-restful'],  # Optional
+    install_requires=['flask', 'flask-restful', 'meinheld'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
