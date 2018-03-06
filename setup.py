877acde2b1fd84e93428c31db56129e85bc2207d
Ryan Lee <ryantlee9@gmail.com>
2018-03-05 22:15:02 -0800

    Bump development status

--- setup.py
+++ setup.py
@@ -80,8 +80,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 2 - Pre-Alpha',
-        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 3 - Alpha',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
